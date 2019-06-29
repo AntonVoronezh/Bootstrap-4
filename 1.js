e915425d310a0a464d79ce369a48fa10415f6ed3
AntonVoronezh <pstut@mail.ru>
2019-06-29 15:29:16 +0300

    iterate 1

--- 1.js
+++ 1.js
@@ -1,5 +1,12 @@
 // Длина объекта
-
 const obj = { a: 1, b: 2, c: 3 };
-
 const length = Object.keys(obj).length;
+
+// Итерация по объекту
+// 1
+
+const userObj = { first: 'aaaa', last: 'bbbbb' };
+
+for(u in userObj) {
+   console.log(u, userObj[u])
+}
